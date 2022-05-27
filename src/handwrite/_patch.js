df9e754028d94c34b4dc6ec7c81a65f2be2922d1
hanxian韩闲 <woow.wu7@gmail.com>
2022-05-27 13:14:11 +0800

    feat: h vnode path6.

--- src/handwrite/_patch.js
+++ src/handwrite/_patch.js
@@ -23,13 +23,48 @@ const _patch = function (oldVnode, newVnode) {
   // 问题：如何判断是否是同一个虚拟节点
   // 回答：( key ) 和 ( css选择器 ) 都要相同
 
-  // a
+  // ------------- a
   // 是同一个虚拟节点
   if (oldVnode.key === newVnode.key && oldVnode.sel === newVnode.sel) {
     console.log("新旧节点-是同一个虚拟节点，则精细化比较");
+
+    // 1
+    // 新旧节点 - 是否是同一个对象
+    // 注意：这种情况表示的是 两个节点完全一样，同一个内存
+    if (oldVnode === newVnode) return;
+
+    // 2
+    // 新节点有text属性，没有children属性
+    if (
+      newVnode.text !== undefined &&
+      (newVnode.children === undefined || newVnode.children.length === 0)
+    ) {
+      console.log("新Vnode有text属性");
+      if (newVnode.text !== oldVnode.text) {
+        oldVnode.elm.innerText = newVnode.text; // 新旧的text不相等
+      }
+    }
+    // 新节点有children
+    else {
+      if (oldVnode.children !== undefined && oldVnode.children.length > 0) {
+        // 新旧节点都有children
+      } else {
+        // 新节点有children，旧节点没有children
+
+        // 1 删除oldVnode的text
+        oldVnode.elm.innerText = "";
+
+        // 2 添加newVnode的children到DOM中
+        // 注意：是先删除oldVnode的text，在添加children
+        for (let i = 0; i < newVnode.children.length; i++) {
+          const DOM = _createElement(newVnode.children[i]);
+          oldVnode.elm.appendChild(DOM);
+        }
+      }
+    }
   }
 
-  // b
+  // ----------- b
   // 不是同一个虚拟节点
   else {
     console.log("新旧节点-不是同一个虚拟节点，暴力插入新节点，删除旧节点");
@@ -43,11 +78,11 @@ const _patch = function (oldVnode, newVnode) {
     // 插入新节点
     // 插入新真实的DOM节点
     if (oldVnode.elm.parentNode && newDOM) {
-      oldVnode.elm.parentNode.insertBefore(newDOM, oldVnode.elm);
+      oldVnode.elm.parentNode.insertBefore(newDOM, oldVnode.elm); // elm属性表示的是真实的DOM
     }
 
     // 删除老节点
-    oldVnode.elm.parentNode.removeChild(oldVnode.elm)
+    oldVnode.elm.parentNode.removeChild(oldVnode.elm);
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import h from "./handwrite/_h.js";
 import patch from "./handwrite/_patch.js";
 
 // h函数可以嵌套
-const vnode = h("ul", {}, [
-  h('li', {}, '1'),
-  h('li', {}, '2')
-]);
-
+const vnode = h("ul", {}, [h("li", {}, "1"), h("li", {}, "2")]);
+const vnode2 = h("ul", {}, "文字");
 // const vnode = h("ul", {}, 'string');
-
 console.log("vnode", vnode);
 
+// 1
+// 首次渲染
 const root = document.getElementById("root");
+patch(root, vnode2);
 
-patch(root, vnode);
+// 2
+// 更新视图
+// button
+const btn = document.createElement("button");
+btn.innerText = "点击，触发path，更新视图";
+document.body.appendChild(btn);
+btn.onclick = () => {
+  console.log("click");
+  // patch(vnode, vnode2);
+  patch(vnode2, vnode);
+};
